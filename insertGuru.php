fcb21a54f230f27f8d52399e48b9801d6bf8421d
Auliya <auliyaa382@gmail.com>
2022-06-02 12:17:58 +0700

    insert

--- insertGuru.php
+++ insertGuru.php
@@ -8,17 +8,24 @@
      $username = $_POST['username'];
      $password = $_POST['password'];
      $hakakses = $_POST['hakakses'];
-    
-    
-    $insertdata="INSERT INTO tbguru(nip, nama_guru, jenis_kelamin, alamat, username) VALUES ($nip,'$nama','$jk','$alamat', '$username')";
-    $sql=mysqli_query($koneksi,$insertdata); 
-        if ($sql){
-            echo "<script type='text/javascript'> alert('Input Data Berhasil'); window.location='viewDataGuru.php';</script>";
-        } else {
-            echo "<script type='text/javascript'> alert('Input Data Gagal'); window.location='addDataGuru.php';</script>";
-        }	
-    $insertdata1="INSERT INTO tbuser(username, password, hakakses) VALUES ('$username','$password','$hakakses')";
-    $sql1=mysqli_query($koneksi,$insertdata1); 
+
+    //  $cek= mysqli_query($koneksi,"SELECT * FROM tbuser where hakakses='GURU'");
+    //  while ($data  = mysqli_fetch_array($cek)){
+    //     $user = $data['username'];
+    //     if($username == $user){
+    //         echo "<script type='text/javascript'> alert('Input Data Gagal'); window.location='addDataGuru.php';</script>";
+    //     }
+    //     exit();
+    //     }
+        $insertdata="INSERT INTO tbguru(nip, nama_guru, jenis_kelamin, alamat, username) VALUES ($nip,'$nama','$jk','$alamat', '$username')";
+            $sql=mysqli_query($koneksi,$insertdata); 
+                if ($sql){
+                    echo "<script type='text/javascript'> alert('Input Data Berhasil'); window.location='viewDataGuru.php';</script>";
+                } else {
+                    echo "<script type='text/javascript'> alert('Input Data Gagal'); window.location='addDataGuru.php';</script>";
+                }	
+            $insertdata1="INSERT INTO tbuser(username, password, hakakses) VALUES ('$username','$password','$hakakses')";
+            $sql1=mysqli_query($koneksi,$insertdata1); 
     }
 
 ?>
