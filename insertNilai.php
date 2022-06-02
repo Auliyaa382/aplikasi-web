421957c09de1dde27f7fc50509e778de675fc1a8
Auliya <auliyaa382@gmail.com>
2022-06-02 13:42:05 +0700

    insert

--- insertNilai.php
+++ insertNilai.php
@@ -8,16 +8,21 @@
      $mapel = $_POST['mapel'];
      $nilai = $_POST['nilai'];
     
-$query = mysqli_query($koneksi, "SELECT max(id_nilai) as idnilai FROM tbnilai");
-$data = mysqli_fetch_array($query);
-$idnilai = $data['idnilai'];
-$urutan = (int) substr($idnilai, 3, 3);
-$urutan++;
-$huruf = "NIL";
-$idnilai = $huruf . sprintf("%03s", $urutan);
+    $query = mysqli_query($koneksi, "SELECT max(id_nilai) as idnilai FROM tbnilai");
+    $data = mysqli_fetch_array($query);
+    $idnilai = $data['idnilai'];
+    $urutan = (int) substr($idnilai, 3, 3);
+    $urutan++;
+    $huruf = "NIL";
+    $idnilai = $huruf . sprintf("%03s", $urutan);
 
+    $cek= mysqli_query($koneksi,"SELECT nis, kode_pelajaran FROM tbnilai where nis='$nis' && kode_pelajaran ='$mapel'");
+    $datauser  = mysqli_num_rows($cek);
+    echo $datauser;
 
-
+    if($datauser >0){
+        echo "<script type='text/javascript'> alert('Input Data Gagal, Mata pelajaran tersebut sudah ditambahkan silahkan cek kembali.'); window.location='nilaiSiswa.php';</script>";
+    }else{
     $insertdata="INSERT INTO tbnilai (id_nilai, nis, kode_pelajaran, nilai) VALUES ('$idnilai','$nis','$mapel','$nilai')";
     $sql=mysqli_query($koneksi,$insertdata); 
         if ($sql){
@@ -25,6 +30,7 @@ $idnilai = $huruf . sprintf("%03s", $urutan);
         } else {
             echo "<script type='text/javascript'> alert('Input Data Gagal');window.location='guruView.php' ;</script>";
         }	
+    }
     }
 
 ?>
